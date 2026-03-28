4e17078e8fbb711134b7cf77799c6269f6346d66
amans-k <srktv6195@gmail.com>
2026-03-28 16:50:10 +0530

    Fix webhook without verifyWebhook

--- server/controllers/webhooks.ts
+++ server/controllers/webhooks.ts
@@ -1,32 +1,32 @@
 import { Request, Response } from "express";
+import { verifyWebhook } from "@clerk/express/webhooks";
 import User from "../models/User.js";
 
 export const clerkWebhook = async (req: Request, res: Response) => {
     try {
-        const evt = req.body;
-        console.log("Webhook received:", evt.type);
+        const evt: any = await verifyWebhook(req);
 
         if (evt.type === "user.created" || evt.type === "user.updated") {
+            const user = await User.findOne({ clerkId: evt.data.id });
+
             const userData = {
                 clerkId: evt.data.id,
-                email: evt.data.email_addresses?.[0]?.email_address,
-                name: (evt.data.first_name || '') + ' ' + (evt.data.last_name || ''),
-                image: evt.data.image_url,
-                role: "user"
+                email: evt.data?.email_addresses[0]?.email_address,
+                name: evt.data?.first_name + " " + evt.data?.last_name,
+                image: evt.data?.image_url,
             };
 
-            await User.findOneAndUpdate(
-                { clerkId: evt.data.id },
-                userData,
-                { upsert: true, new: true }
-            );
-            
-            console.log("User saved:", userData.clerkId);
+            if (user) {
+                await User.findOneAndUpdate({ clerkId: evt.data.id }, userData);
+            } else {
+                await User.create(userData);
+            }
         }
 
         return res.json({ success: true, message: "Webhook received" });
     } catch (err) {
-        console.error("Error processing webhook:", err);
-        return res.status(400).json({ success: false, message: "Error processing webhook" });
+        console.error("Error verifying webhook:", err);
+        return res.status(400).json({ success: false, message: "Error verifying webhook" });
     }
 };
+
